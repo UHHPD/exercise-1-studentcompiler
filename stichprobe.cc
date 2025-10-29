d01ded946a9df417cc69cc243be2c57dab71fa30
Mahtab Mirzaei <mhtbmrz@dhcp-172-21-17-223.wlan.uni-hamburg.de.local>
2025-10-29 16:43:04 +0100

    stichprobe_1

---
+++ stichprobe.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+
+int main(){
+
+    std::ifstream fin("datensumme.txt");
+
+    const int N = 234;
+    double a[N];
+    double a_i;
+    int i = 0;
+    double sum = 0;
+
+    while (fin >> a_i && i < N){
+        a[i] = a_i;
+        sum += a_i;
+        i++;
+    }
+
+    double mean = sum / N;
+
+    double var = 0;
+
+    for (int i = 0; i < N; i++){
+        var += pow(a[i] - mean ,2);
+    }
+
+    var /= N;
+
+    std::cout << sum << std::endl;
+    std::cout << mean << std::endl;
+    std::cout << var << std::endl;
+}
